1cf36a8594b6a5472db51c364462a559dbf16931
root <you@example.com>
2023-02-08 18:47:50 -0800

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,12 +7,8 @@ Implemented using recursion
 import re
 import requests
 import sys
-
-
 API = "https://jsonplaceholder.typicode.com"
 """REST API url"""
-
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
